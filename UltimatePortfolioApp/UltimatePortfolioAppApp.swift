5adfb6c7cf71bbcc2144608df2a326d0ca5c0f00
luke.inger@jcb-finance.co.uk <luke.inger@jcb-finance.co.uk>
2021-07-26 20:32:11 +0100

    Initial Commit

---
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioAppApp.swift
+//  UltimatePortfolioApp
+//
+//  Created by Luke Inger on 26/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
